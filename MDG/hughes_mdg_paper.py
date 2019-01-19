6391fa4bc4697e83753cb13ac6304bef9caaf881
volpatto <volpatto@lncc.br>
2019-01-19 02:26:08 -0200

    Add first version of Hughes mixed discontinuous Galerkin

--- MDG/hughes_mdg_paper.py
+++ MDG/hughes_mdg_paper.py
@@ -1,16 +1,15 @@
 from firedrake import *
-import numpy as np
-import random
 try:
     import matplotlib.pyplot as plt
     plt.rcParams['contour.corner_mask'] = False
+    plt.close('all')
 except:
     warning("Matplotlib not imported")
 
-nx, ny = 100, 100
+nx, ny = 20, 20
 Lx, Ly = 1.0, 1.0
 quadrilateral = True
-mesh = UnitSquareMesh(nx, ny, quadrilateral=quadrilateral)
+mesh = RectangleMesh(nx, ny, Lx, Ly, quadrilateral=quadrilateral)
 
 if quadrilateral:
     pressure_family = 'DQ'
@@ -20,10 +19,14 @@ else:
 plot(mesh)
 plt.axis('off')
 
-degree = 2
+degree = 1
 U = VectorFunctionSpace(mesh, "DG", degree)
 V = FunctionSpace(mesh, pressure_family, degree)
 W = MixedFunctionSpace([U, V])
+
+v, p = TrialFunctions(W)
+w, q = TestFunctions(W)
+solution = Function(W)
 
 n = FacetNormal(mesh)
 x, y = SpatialCoordinate(mesh)
@@ -39,63 +42,55 @@ f = Function(V).interpolate(source_expr)
 plot(sol_exact)
 plt.axis('off')
 
-v, p = TrialFunctions(wSpace)
-w, q = TestFunctions(wSpace)
-solution = Function(wSpace)
+# Model parameters
+k = Constant(1.0)
+mu = Constant(1.0)
+rho = Constant(0.0)
+g = Constant((0.0, 0.0))
 
 # Boundaries: Left (1), Right (2), Bottom(3), Top (4)
 vx = -2 * pi / Lx * cos(2 * pi * x / Lx) * sin(2 * pi * y / Ly)
 vy = -2 * pi / Ly * sin(2 * pi * x / Lx) * cos(2 * pi * y / Ly)
 p_boundaries = Constant(0.0)
-bc1 = DirichletBC(W[0], as_vector([vx, 0.0]), 1)
-bc2 = DirichletBC(W[0], as_vector([vx, 0.0]), 2)
-bc3 = DirichletBC(W[0], as_vector([0.0, vy]), 3)
-bc4 = DirichletBC(W[0], as_vector([0.0, vy]), 4)
-bcs = [bc1, bc2, bc3, bc4]
+v_projected = sigma_e
+# An alternative for the BC
+# v_projected = project(as_vector([vx, vy]), W[0])
 
-#########################################################
-#########################################################
-#########################################################
-
-h = CellSize(mesh)
+# Stabilizing parameters
+h = CellDiameter(mesh)
 h_avg = (h('+') + h('-')) / 2.
+beta = Constant(1.0)
 
-eta_p = Constant(0.0)
-
-a = dot(w, alpha(conc_k) * v) * dx - \
+# Regular DG terms
+a = dot(w, (mu / k) * v) * dx - \
     div(w) * p * dx + \
     q * div(v) * dx + \
     jump(w, n) * avg(p) * dS - \
     avg(q) * jump(v, n) * dS + \
-    (dot(w, n) * p - dot(v, n) * q) * (ds(3) + ds(4)) + \
-    0.5 * dot(alpha(conc_k) * w - grad(q), \
-             invalpha(conc_k) * (alpha(conc_k) * v + grad(p))) * dx + \
-    (eta_p / h_avg) * avg(1. / alpha(conc_k)) * dot(jump(q, n), jump(p, n)) * dS
-
-#########################################################
-#########################################################
-#########################################################
-
-a = (dot(sigma, tau) - div(tau) * u + v * div(sigma)) * dx
-a += 0.5 * inner(sigma + grad(u), - tau + grad(v)) * dx
-L = f * v * dx - Constant(0.0) * dot(tau, n) * (ds(1) + ds(2) + ds(3) + ds(4))
+    (dot(w, n) * p - dot(v, n) * q) * (ds(1) + ds(2) + ds(3) + ds(4))
+L = f * q * dx - dot(rho * g, w) * dx - p_boundaries * dot(w, n) * (ds(1) + ds(2) + ds(3) + ds(4)) - \
+    dot(v_projected, n) * q * (ds(1) + ds(2)) - dot(v_projected, n) * q * (ds(3) + ds(4))
+# Stabilizing terms
+a += 0.5 * dot(- (mu / k) * w + grad(q), (k / mu) * ((mu / k) * v + grad(p))) * dx + \
+    (beta / h_avg) * avg(k / mu) * dot(jump(q, n), jump(p, n)) * dS
+L += 0.5 * dot((k / mu) * rho * g, - (mu / k) * w + grad(q)) * dx
 
 solver_parameters = {
-    #'ksp_type': 'tfqmr',
+    # 'ksp_type': 'tfqmr',
     'ksp_type': 'gmres',
     'pc_type': 'bjacobi',
     'mat_type': 'aij',
     'ksp_rtol': 1e-3,
     'ksp_max_it': 2000,
-    'ksp_monitor': True
+    'ksp_monitor': False
 }
 
-solve(a == L, solution, bcs=bcs, solver_parameters=solver_parameters)
+solve(a == L, solution, solver_parameters=solver_parameters)
 sigma_h, u_h = solution.split()
 sigma_h.rename('Velocity', 'label')
 u_h.rename('Pressure', 'label')
 
-output = File('hughes_paper.pvd', project_output=True)
+output = File('hughes_paper_MDG.pvd', project_output=True)
 output.write(sigma_h, u_h, sol_exact, sigma_e)
 
 plot(sigma_h)
