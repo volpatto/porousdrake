314d5d0347a15fb9e95d7751d9e81cafae90262b
volpatto <volpatto@lncc.br>
2019-02-01 01:09:03 -0200

    Add DPP convergence study

---
+++ DPP-convergence/cg-jh.py
@@ -0,0 +1,121 @@
+"""
+The present code is based in the code and method proposed in [1], but adapted from [2]. Here I just use as a reference
+code, since the mentioned paper is a benchmark for my own work.
+
+Ref:
+[1] S.H.S. Joodat, K.B. Nakshatrala, R. Ballarini, Modeling flow in porous media with double porosity/permeability:
+A stabilized mixed formulation, error analysis, and numerical solutions, Computer Methods in Applied Mechanics and
+Engineering, Volume 337, 2018, Pages 632-676, ISSN 0045-7825, https://doi.org/10.1016/j.cma.2018.04.004.
+[2] Joshaghani, M. S., S. H. S. Joodat, and K. B. Nakshatrala. "A stabilized mixed discontinuous Galerkin formulation
+for double porosity/permeability model." arXiv preprint arXiv:1805.01389 (2018).
+"""
+from firedrake import *
+import numpy as np
+try:
+    import matplotlib.pyplot as plt
+except:
+    warning("Matplotlib not imported")
+
+nx, ny = 20, 20
+Lx, Ly = 1.0, 1.0
+quadrilateral = True
+mesh = RectangleMesh(nx, ny, Lx, Ly, quadrilateral=quadrilateral)
+
+# Mesh entities
+n = FacetNormal(mesh)
+x, y = SpatialCoordinate(mesh)
+h = CellDiameter(mesh)
+
+degree = 1
+velSpace = VectorFunctionSpace(mesh, "CG", degree)
+pSpace = FunctionSpace(mesh, "CG", degree)
+wSpace = MixedFunctionSpace([velSpace, pSpace, velSpace, pSpace])
+
+mu0 = Constant(1.0)
+k1 = Constant(1.0)
+k2 = Constant(0.1)
+b_factor = Constant(1.0)
+
+
+def alpha1():
+    return mu0 / k1
+
+
+def invalpha1():
+    return 1. / alpha1()
+
+
+def alpha2():
+    return mu0 / k2
+
+
+def invalpha2():
+    return 1. / alpha2()
+
+
+# Exact solution and source term projection
+eta = sqrt(b_factor * (k1 + k2) / (k1 * k2))
+p_exact_1 = mu0 / pi * exp(pi * x) * sin(pi * y) - mu0 / (b_factor * k1) * exp(eta * y)
+p_exact_2 = mu0 / pi * exp(pi * x) * sin(pi * y) + mu0 / (b_factor * k2) * exp(eta * y)
+p_e_1 = Function(pSpace).interpolate(p_exact_1)
+p_e_1.rename('Exact macro pressure', 'label')
+p_e_2 = Function(pSpace).interpolate(p_exact_2)
+p_e_2.rename('Exact micro pressure', 'label')
+v_e_1 = Function(velSpace, name='Exact macro velocity')
+v_e_1.project(-(k1 / mu0) * grad(p_e_1))
+v_e_2 = Function(velSpace, name='Exact macro velocity')
+v_e_2.project(-(k2 / mu0) * grad(p_e_2))
+
+plot(v_e_1)
+plot(v_e_2)
+
+(v1, p1, v2, p2) = TrialFunctions(wSpace)
+(w1, q1, w2, q2) = TestFunctions(wSpace)
+DPP_solution = Function(wSpace)
+
+rhob1, rhob2 = Constant((0.0, 0.0)), Constant((0.0, 0.0))
+f = Constant(0.0)
+
+aDPP = dot(w1, alpha1() * v1) * dx + \
+    dot(w2, alpha2() * v2) * dx - \
+    div(w1) * p1 * dx - \
+    div(w2) * p2 * dx + \
+    q1 * div(v1) * dx + \
+    q2 * div(v2) * dx + \
+    q1 * (b_factor * invalpha1() / k1) * (p1 - p2) * dx - \
+    q2 * (b_factor * invalpha2() / k2) * (p1 - p2) * dx - \
+    0.5 * dot(alpha1() * w1 - grad(q1), invalpha1() * (alpha1() * v1 + grad(p1))) * dx - \
+    0.5 * dot(alpha2() * w2 - grad(q2), invalpha2() * (alpha2() * v2 + grad(p2))) * dx
+LDPP = dot(w1, rhob1) * dx + \
+    dot(w2, rhob2) * dx - \
+    dot(w1, n) * p_e_1 * ds - \
+    dot(w2, n) * p_e_2 * ds - \
+    0.5 * dot(alpha1() * w1 - grad(q1), invalpha1() * rhob1) * dx - \
+    0.5 * dot(alpha2() * w2 - grad(q2), invalpha2() * rhob2) * dx
+
+solver_parameters = {
+    'ksp_type': 'lgmres',
+    'pc_type': 'lu',
+    'mat_type': 'aij',
+    'ksp_rtol': 1e-5,
+    'ksp_monitor_true_residual': True
+}
+
+problem_flow = LinearVariationalProblem(aDPP, LDPP, DPP_solution, bcs=[], constant_jacobian=False)
+solver_flow = LinearVariationalSolver(problem_flow, options_prefix='flow_', solver_parameters=solver_parameters)
+solver_flow.solve()
+
+plot(DPP_solution.sub(0))
+plot(DPP_solution.sub(2))
+plt.show()
+
+output_file = File('results_cg-jh/dpp_exact.pvd')
+v1_sol = DPP_solution.sub(0)
+v1_sol.rename('Macro velocity', 'label')
+p1_sol = DPP_solution.sub(1)
+p1_sol.rename('Macro pressure', 'label')
+v2_sol = DPP_solution.sub(2)
+v2_sol.rename('Micro velocity', 'label')
+p2_sol = DPP_solution.sub(3)
+p2_sol.rename('Micro pressure', 'label')
+output_file.write(p1_sol, v1_sol, p2_sol, v2_sol, p_e_1, v_e_1, p_e_2, v_e_2)
---
+++ DPP-convergence/cgls.py
@@ -0,0 +1,142 @@
+from firedrake import *
+import numpy as np
+from firedrake.petsc import PETSc
+from firedrake import COMM_WORLD
+try:
+    import matplotlib.pyplot as plt
+    plt.rcParams['contour.corner_mask'] = False
+    plt.close('all')
+except:
+    warning("Matplotlib not imported")
+
+nx, ny = 20, 20
+Lx, Ly = 1.0, 1.0
+quadrilateral = True
+mesh = RectangleMesh(nx, ny, Lx, Ly, quadrilateral=quadrilateral)
+
+plot(mesh)
+plt.axis('off')
+
+degree = 1
+pressure_family = 'CG'
+velocity_family = 'CG'
+U = VectorFunctionSpace(mesh, velocity_family, degree)
+V = FunctionSpace(mesh, pressure_family, degree)
+W = U * V * U * V
+
+# Trial and test functions
+DPP_solution = Function(W)
+u1, p1, u2, p2 = TrialFunctions(W)
+v1, q1, v2, q2 = TestFunctions(W)
+
+# Mesh entities
+n = FacetNormal(mesh)
+x, y = SpatialCoordinate(mesh)
+h = CellDiameter(mesh)
+
+#################################################
+# *** Model parameters
+#################################################
+mu0 = Constant(1.0)
+k1 = Constant(1.0)
+k2 = Constant(0.1)
+b_factor = Constant(1.0)
+
+
+def alpha1():
+    return mu0 / k1
+
+
+def invalpha1():
+    return 1. / alpha1()
+
+
+def alpha2():
+    return mu0 / k2
+
+
+def invalpha2():
+    return 1. / alpha2()
+
+
+#################################################
+#################################################
+#################################################
+
+# Exact solution and source term projection
+eta = sqrt(b_factor * (k1 + k2) / (k1 * k2))
+p_exact_1 = mu0 / pi * exp(pi * x) * sin(pi * y) - mu0 / (b_factor * k1) * exp(eta * y)
+p_exact_2 = mu0 / pi * exp(pi * x) * sin(pi * y) + mu0 / (b_factor * k2) * exp(eta * y)
+p_e_1 = Function(W.sub(1)).interpolate(p_exact_1)
+p_e_1.rename('Exact macro pressure', 'label')
+p_e_2 = Function(W.sub(3)).interpolate(p_exact_2)
+p_e_2.rename('Exact micro pressure', 'label')
+v_e_1 = Function(U, name='Exact macro velocity')
+v_e_1.project(-(k1 / mu0) * grad(p_e_1))
+v_e_2 = Function(U, name='Exact macro velocity')
+v_e_2.project(-(k2 / mu0) * grad(p_e_2))
+
+plot(p_e_1)
+plot(p_e_2)
+
+# Source term
+rhob1, rhob2 = Constant((0.0, 0.0)), Constant((0.0, 0.0))
+f = Constant(0.0)
+
+# Stabilizing parameter
+delta_0 = Constant(1.0)
+delta_1 = Constant(0.5)
+delta_2 = Constant(0.5)
+delta_3 = Constant(0.0)
+
+# Mixed classical terms
+a = (dot(alpha1() * u1, v1) - div(v1) * p1 - delta_0 * q1 * div(u1)) * dx
+a += (dot(alpha2() * u2, v2) - div(v2) * p2 - delta_0 * q2 * div(u2)) * dx
+a += delta_0 * q1 * (b_factor * invalpha1() / k1) * (p2 - p1) * dx
+a += delta_0 * q2 * (b_factor * invalpha2() / k2) * (p1 - p2) * dx
+L = - delta_0 * dot(rhob1, v1) * dx
+L += - delta_0 * dot(rhob2, v2) * dx
+# Stabilizing terms
+###
+a += delta_1 * inner(invalpha1() * (alpha1() * u1 + grad(p1)), delta_0 * alpha1() * v1 + grad(q1)) * dx
+a += delta_1 * inner(invalpha2() * (alpha2() * u2 + grad(p2)), delta_0 * alpha2() * v2 + grad(q2)) * dx
+###
+a += delta_2 * alpha1() * div(u1) * div(v1) * dx
+a += delta_2 * alpha2() * div(u2) * div(v2) * dx
+a += -delta_2 * alpha1() * (b_factor * invalpha1() / k1) * (p2 - p1) * div(v1) * dx
+a += -delta_2 * alpha2() * (b_factor * invalpha2() / k2) * (p1 - p2) * div(v2) * dx
+###
+a += delta_3 * inner(invalpha1() * curl(alpha1() * u1), curl(alpha1() * v1)) * dx
+a += delta_3 * inner(invalpha2() * curl(alpha2() * u2), curl(alpha2() * v2)) * dx
+# Weakly imposed BC
+L += -dot(v1, n) * p_e_1 * ds - \
+     dot(v2, n) * p_e_2 * ds
+
+#  Solving
+solver_parameters = {
+    'ksp_type': 'lgmres',
+    'pc_type': 'lu',
+    'mat_type': 'aij',
+    'ksp_rtol': 1e-5,
+    'ksp_atol': 1e-5,
+    'ksp_monitor_true_residual': True
+}
+problem_flow = LinearVariationalProblem(a, L, DPP_solution, bcs=[], constant_jacobian=False)
+solver_flow = LinearVariationalSolver(problem_flow, options_prefix='dpp_flow', solver_parameters=solver_parameters)
+solver_flow.solve()
+
+# Writing solution in a .vtk file and plotting the solution
+plot(DPP_solution.sub(1))
+plot(DPP_solution.sub(3))
+plt.show()
+
+output_file = File('results_cgls/dpp_exact.pvd'.format(fileName))
+v1_sol = DPP_solution.sub(0)
+v1_sol.rename('Macro velocity', 'label')
+p1_sol = DPP_solution.sub(1)
+p1_sol.rename('Macro pressure', 'label')
+v2_sol = DPP_solution.sub(2)
+v2_sol.rename('Micro velocity', 'label')
+p2_sol = DPP_solution.sub(3)
+p2_sol.rename('Micro pressure', 'label')
+output_file.write(p1_sol, v1_sol, p2_sol, v2_sol, p_e_1, v_e_1, p_e_2, v_e_2)
