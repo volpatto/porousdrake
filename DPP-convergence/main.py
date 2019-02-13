2e710572fad87efc3bf99ce5c37ad891f08be154
volpatto <volpatto@lncc.br>
2019-02-13 01:38:51 -0200

    WIP: minor changes

--- DPP-convergence/main.py
+++ DPP-convergence/main.py
@@ -15,24 +15,26 @@ degree = 1
 mesh = RectangleMesh(nx, ny, Lx, Ly, quadrilateral=quadrilateral)
 
 # Stabilizing parameters
-delta_0 = Constant(-1)
-delta_1 = Constant(0.5)
-delta_2 = Constant(0.0)
+delta_0 = Constant(1)
+delta_1 = Constant(-0.5)
+delta_2 = Constant(0.5)
 delta_3 = Constant(0.0)
 eta_u = Constant(10.0)
 eta_p = 100 * eta_u
-beta_0 = Constant(1.0)
+beta_0 = Constant(0.5)
+
+# Choosing the solver
+solver = solvers.sdhm
 
 # Cold run
-p1_sol, v1_sol, p2_sol, v2_sol, p_e_1, v_e_1, p_e_2, v_e_2 = solvers.dgls(
+p1_sol, v1_sol, p2_sol, v2_sol, p_e_1, v_e_1, p_e_2, v_e_2 = solver(
     mesh=mesh,
     degree=degree,
     delta_0=delta_0,
     delta_1=delta_1,
     delta_2=delta_2,
     delta_3=delta_3,
-    eta_u=eta_u,
-    eta_p=eta_p
+    beta_0=beta_0
 )
 plot(p1_sol)
 plot(p_e_1)
@@ -46,16 +48,15 @@ plt.show()
 print('*** Cold run OK ***\n')
 
 convergence.convergence_hp(
-    solvers.dgls,
+    solver,
     min_degree=degree,
     max_degree=degree + 1,
     mesh_pow_min=3,
     mesh_pow_max=7,
-    quadrilateral=True,
+    quadrilateral=quadrilateral,
     delta_0=delta_0,
     delta_1=delta_1,
     delta_2=delta_2,
     delta_3=delta_3,
-    eta_u=eta_u,
-    eta_p=eta_p
+    beta_0=beta_0
 )
