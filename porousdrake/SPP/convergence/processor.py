564a8a41a06e68fe30d8dc888384a366783563cb
volpatto <volpatto@lncc.br>
2020-03-04 01:11:15 -0300

    New csv output for convergence rate

--- porousdrake/SPP/convergence/processor.py
+++ porousdrake/SPP/convergence/processor.py
@@ -2,6 +2,7 @@ from firedrake import *
 from firedrake.petsc import PETSc
 from firedrake import COMM_WORLD
 import numpy as np
+import pandas as pd
 from scipy.stats import linregress
 import os
 import porousdrake.SPP.convergence.exact_solution as sol
@@ -32,14 +33,24 @@ def convergence_hp(
     min_degree=1,
     max_degree=4,
     numel_xy=(2, 4, 8, 16, 32, 64, 128, 256),
-    norm_type="L2",
     quadrilateral=True,
     name="",
     **kwargs
 ):
+    computed_errors_dict = {
+        "Element": list(),
+        "Degree": list(),
+        "Cells": list(),
+        "Mesh size": list(),
+        "l2-error p": list(),
+        "l2-error u": list(),
+        "Hdiv-error u": list(),
+    }
+    element_kind = "Quad" if quadrilateral else "Tri"
     for degree in range(min_degree, max_degree):
         p_errors = np.array([])
         v_errors = np.array([])
+        v_errors_hdiv = np.array([])
         num_cells = np.array([])
         mesh_size = np.array([])
         for n in numel_xy:
@@ -49,36 +60,45 @@ def convergence_hp(
             mesh_size = np.append(mesh_size, 1.0 / n)
 
             p_sol, v_sol, p_e, v_e = solver(mesh=mesh, degree=degree, **kwargs)
-            error_dictionary = {}
-            error_dictionary.update(compute_error(p_sol, p_e, "p_error", norm_type=norm_type))
-            p_errors = np.append(p_errors, error_dictionary["p_error"])
-            error_dictionary.update(compute_error(v_sol, v_e, "v_error"), norm_type=norm_type)
-            v_errors = np.append(v_errors, error_dictionary["v_error"])
-        p_errors_log2 = np.log2(p_errors)
-        v_errors_log2 = np.log2(v_errors)
-        num_cells_log2 = np.log2(num_cells)
-        mesh_size_log2 = np.log2(mesh_size)
-        p_slope, intercept, r_value, p_value, stderr = linregress(mesh_size_log2, p_errors_log2)
+
+            current_error_p = compute_error(p_sol, p_e, "p_error", norm_type="L2")
+            p_errors = np.append(p_errors, current_error_p)
+
+            current_error_v = compute_error(v_sol, v_e, "v_error", norm_type="L2")
+            v_errors = np.append(v_errors, current_error_v)
+
+            current_error_v_hdiv = compute_error(v_sol, v_e, "v_error_hdiv", norm_type="Hdiv")
+            v_errors_hdiv = np.append(v_errors_hdiv, current_error_v_hdiv)
+
+            computed_errors_dict["Element"].append(element_kind)
+            computed_errors_dict["Degree"].append(degree)
+            computed_errors_dict["Cells"].append(num_cells)
+            computed_errors_dict["Mesh size"].append(mesh_size)
+            computed_errors_dict["l2-error p"].append(current_error_p)
+            computed_errors_dict["l2-error u"].append(current_error_v)
+            computed_errors_dict["Hdiv-error u"].append(current_error_v_hdiv)
+
+        p_errors_log10 = np.log10(p_errors)
+        v_errors_log10 = np.log10(v_errors)
+        num_cells_log10 = np.log10(num_cells)
+        mesh_size_log10 = np.log10(mesh_size)
+        p_slope, intercept, r_value, p_value, stderr = linregress(mesh_size_log10, p_errors_log10)
         PETSc.Sys.Print(
             "\n--------------------------------------\nDegree %d: p slope error %f"
             % (degree, np.abs(p_slope))
         )
         v_slope, intercept_v, r_value_v, p_value_v, stderr_v = linregress(
-            mesh_size_log2, v_errors_log2
+            mesh_size_log10, v_errors_log10
         )
         PETSc.Sys.Print(
             "\n--------------------------------------\nDegree %d: v slope error %f"
             % (degree, np.abs(v_slope))
         )
-        # _plot_errors(mesh_size, p1_errors, p1_slope, degree, name='p1_errors')
-        # _plot_errors(mesh_size, p2_errors, p2_slope, degree, name='p2_errors')
-        # _plot_errors(mesh_size, v1_errors, v1_slope, degree, name='v1_errors')
-        # _plot_errors(mesh_size, v2_errors, v2_slope, degree, name='v2_errors')
-        os.makedirs("results_%s" % name, exist_ok=True)
-        np.savetxt(
-            ("results_%s/errors_degree%d.dat" % (name, degree)),
-            np.transpose([-mesh_size_log2, p_errors_log2, v_errors_log2]),
-        )
+
+    os.makedirs("results_%s" % name, exist_ok=True)
+    df_computed_errors = pd.DataFrame(data=computed_errors_dict)
+    path_to_save_results = "results_%s/errors.csv" % name
+    df_computed_errors.to_csv(path_to_save_results)
 
     return
 
